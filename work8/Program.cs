85fa8ad126983339a12573a542027f157da0396e
RusElfimov <ruselfimovbox@outlook.com>
2022-10-04 17:59:02 +0300

    work8

---
+++ work8/Program.cs
@@ -0,0 +1,7 @@
+﻿//Показать вторую цифру трехзначного числа.Console.WriteLine("Введите трехзначное число");
+Console.WriteLine("Введите трехзначное число");
+int n = int.Parse(Console.ReadLine());
+int first = n/100;
+int second = n%100;
+int third = second/10;
+Console.Write("Вторая цифра числа - " + third);
