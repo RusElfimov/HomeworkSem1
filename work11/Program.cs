fc8da9b9e0de44a0ee933ef1cc632d980bbef4a5
RusElfimov <ruselfimovbox@outlook.com>
2022-10-04 18:17:54 +0300

    work11

---
+++ work11/Program.cs
@@ -0,0 +1,16 @@
+﻿//Выяснить,кратно ли число заданному,если нет ,вывести остаток.
+Console.WriteLine("Введите  число х");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число y");
+int y = int.Parse(Console.ReadLine());
+int z = x%y;
+
+if (x%y==0)
+{
+    Console.Write(x + " кратно " + y);
+}
+else 
+{
+    Console.WriteLine(x + " не кратно " + y);
+    Console.Write("Остаток от деления " + z);
+}
