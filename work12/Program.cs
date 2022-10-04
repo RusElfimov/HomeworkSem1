4d84b629f4b207e89efb7b381ac308c559d41cf9
RusElfimov <ruselfimovbox@outlook.com>
2022-10-04 18:22:29 +0300

    work12

---
+++ work12/Program.cs
@@ -0,0 +1,13 @@
+﻿//Найти третью цифру числа или сообщить ,что ее нет.
+string a = Console.ReadLine();
+int b = a.Length;
+if (b < 3 )
+{
+     Console.WriteLine("третьей цифры числа нет");
+    
+}
+else 
+{
+    Console.WriteLine(a[2]);
+}
+
