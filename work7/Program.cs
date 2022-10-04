3a707ae0adfae3b7feb93ee8d1f8f23f72b898d1
RusElfimov <ruselfimovbox@outlook.com>
2022-10-04 17:40:46 +0300

    work7

---
+++ work7/Program.cs
@@ -0,0 +1,5 @@
+﻿//Показать последнюю цифру трезначного числа.
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int x = n%10;
+Console.WriteLine("Последняя цифра числа - " + x);
