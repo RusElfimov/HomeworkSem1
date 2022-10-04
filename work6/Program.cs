33541ba9fed5c788d486ec23348ebf208b4d652b
RusElfimov <ruselfimovbox@outlook.com>
2022-10-04 14:37:07 +0300

    work6

---
+++ work6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать четные числа от 1 до N.
+Console.WriteLine("Введите N");
+int N = int.Parse(Console.ReadLine());
+int N1 = 1;
+
+
+while (N1<=N)
+    {
+        int x = N1++;
+        if (x%2==0)
+        Console.WriteLine(x);
+    }
+     
+
+
