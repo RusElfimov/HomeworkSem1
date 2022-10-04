2f5a05b0545255aeae5680f1642da48e60b27efb
RusElfimov <ruselfimovbox@outlook.com>
2022-10-04 17:56:51 +0300

    work9

---
+++ work9/Program.cs
@@ -0,0 +1,17 @@
+﻿//Дано число из отрезка [10,99].Показать наибольшую цифру числа.
+Console.WriteLine("Введите число от 10 до 99");
+int n = int.Parse(Console.ReadLine());
+int first = n/10;
+int second = n%10;
+if (first>second)
+{
+    Console.Write(first);
+}
+else if (first<second)
+{
+    Console.Write(second);
+}
+else 
+{
+    Console.Write("Цифры равны");
+}
