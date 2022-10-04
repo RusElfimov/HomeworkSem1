9b4a8c5ebe9ee7523c6a30405677edb3615f3d51
RusElfimov <ruselfimovbox@outlook.com>
2022-10-04 18:10:15 +0300

    work10

---
+++ work10/Program.cs
@@ -0,0 +1,7 @@
+﻿//Удалить вторую цифру трехзначного числа.
+Console.WriteLine("Введите трехзначное число");
+int n = int.Parse(Console.ReadLine());
+int n1 = n/100;
+int n2 = n%10;
+Console.Write(n1);
+Console.Write(n2);
