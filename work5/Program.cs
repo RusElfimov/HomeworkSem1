9cedc16329e4ae519781f7ccc19a671661450d2c
RusElfimov <ruselfimovbox@outlook.com>
2022-10-04 14:29:06 +0300

    work5

---
+++ work5/Program.cs
@@ -0,0 +1,10 @@
+﻿//Показать числа от -N до N.
+int N = Convert.ToInt32(Console.ReadLine());
+int startNumber = N;
+
+        while(startNumber >= -N)
+    {        
+        Console.Write(startNumber + ", ");
+        startNumber --;
+    }
+
