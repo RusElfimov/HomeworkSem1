15ea0f9ba7aae4c37b283b76d56541110304eca7
RusElfimov <ruselfimovbox@outlook.com>
2022-10-04 12:48:52 +0300

    work1

---
+++ work1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+
+if (a==b*b)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
