11b764b8c381362042bdda90085842a628eb5904
RusElfimov <ruselfimovbox@outlook.com>
2022-10-04 13:38:40 +0300

    work4

---
+++ work4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Выяснить является ли число четным
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if (number%2==0)
+{
+    Console.WriteLine("Четное");
+}
+else 
+{
+    Console.WriteLine("Нечетное");
+}
